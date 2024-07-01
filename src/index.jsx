c8f1798599aa698f042d45ad5e8a49996f697364
TirkarParth <161726354+TirkarParth@users.noreply.github.com>
2024-07-02 00:14:49 +0200

    Wrap MainView with Bootstrap Container for responsive layout
    Imported Bootstrap CSS and Container component.
    Wrapped the MainView component with Container for a responsive layout.
    Ensured consistent styling across the application by including Bootstrap's base styles.

--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { MainView } from './components/main-view/main-view';
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap CSS
+import './index.scss'; // Import custom SCSS
 
-// Below line with import MainView function from the path /components/main-view/main-view , the file extension jsx for main-view.jsx doesn't not need to be added for jsx code.
-import { MainView } from "./components/main-view/main-view";
-
-// Import statement to indicate that you need to bundle `./index.scss`
-import "./index.scss";
-
-// Main component (will eventually use all the others)
-
-//       const MyFlixApplication = () => {
-//     return (
-//         <div className="my-flix">
-//             <div>Good morning</div>
-//         </div>
-//     );
-// };
-
-const App = () => {
-    return <MainView />;
+const MyFlixApplication = () => {
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
 };
 
-
-// Finds the root of your app
-const container = document.querySelector("#root");
-const root = createRoot(container);
-
-// Tells React to render your app in the root DOM element
-
-// root.render(<MyFlixApplication />);
-root.render(<App />);
+ReactDOM.render(<MyFlixApplication />, document.getElementById('root'));
